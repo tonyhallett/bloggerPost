39caf8a852924f906194412783e52de15848f6be
Tony Hallett <tonyhallett74@gmail.com>
2019-09-16 15:31:04 +0100

    code formatting indent

--- src/lib/oauthAuthenticateClient.ts
+++ src/lib/oauthAuthenticateClient.ts
@@ -19,34 +19,34 @@ export function authenticateClient(oAuth2Client:OAuth2Client,freePort:number,sco
         // Open an http server to accept the oauth callback. In this simple example, the
         // only request to our webserver is to /oauth2callback?code=<code>
         const server = http.createServer(async (req:any, res:any) => {
-        if (req.url.indexOf(`state=${state}`) > -1) {
-            console.log("received server response");
-            const qs = querystring.parse(url.parse(req.url).query);
-            const errorMessage=qs.error;
-            const successMessage='Authentication successful! Please return to the console.'
-            const responseMessage=errorMessage?errorMessage:successMessage
-            res.end(responseMessage);
-            server.close();
-            if(errorMessage){
-                console.groupEnd();
-                reject(new Error("Authentication failed: " + errorMessage));
-            }else{
-                // Now that we have the code, use that to acquire tokens.
-                console.log("getting tokens...")
-                let r= await oAuth2Client.getToken(qs.code)
-                // Make sure to set the credentials on the OAuth2 client.
-                
-                oAuth2Client.setCredentials(r.tokens);
-                console.info('Tokens acquired and client authenticated.');
-                console.groupEnd();
-                resolve();
+            if (req.url.indexOf(`state=${state}`) > -1) {
+                console.log("received server response");
+                const qs = querystring.parse(url.parse(req.url).query);
+                const errorMessage=qs.error;
+                const successMessage='Authentication successful! Please return to the console.'
+                const responseMessage=errorMessage?errorMessage:successMessage
+                res.end(responseMessage);
+                server.close();
+                if(errorMessage){
+                    console.groupEnd();
+                    reject(new Error("Authentication failed: " + errorMessage));
+                }else{
+                    // Now that we have the code, use that to acquire tokens.
+                    console.log("getting tokens...")
+                    let r= await oAuth2Client.getToken(qs.code)
+                    // Make sure to set the credentials on the OAuth2 client.
+                    
+                    oAuth2Client.setCredentials(r.tokens);
+                    console.info('Tokens acquired and client authenticated.');
+                    console.groupEnd();
+                    resolve();
+                }
             }
-        }
         }).listen(freePort, () => {
             console.log("listening for server response on port: " + freePort);
             console.log('opening the browser');
-        // open the browser to the authorize url to start the workflow
-        opn(authorizeUrl);
+            // open the browser to the authorize url to start the workflow
+            opn(authorizeUrl);
         });
     });
     }
